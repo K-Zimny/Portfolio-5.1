0f4c67182e2155f5c41e4920e9acef97c441ba94
kzim <k.zimny1994@gmail.com>
2022-05-03 20:37:35 -0500

    created effect 3

---
+++ js/hero.js
@@ -0,0 +1,185 @@
+// Hero items brought into const
+
+$(document).ready(function () {
+  console.log("ready!");
+
+  //   const heroItem1 = $("jsHeroItem1 >");
+  //   const heroItem2 = document.getElementById("jsHeroItem2");
+  //   const heroItem3 = document.getElementById("jsHeroItem3");
+  //   const heroItem4 = document.getElementById("jsHeroItem4");
+  //   const heroItem5 = document.getElementById("jsHeroItem5");
+  //   const heroItem6 = document.getElementById("jsHeroItem6");
+  //   const heroItem7 = document.getElementById("jsHeroItem7");
+  //   const heroItem8 = document.getElementById("jsHeroItem8");
+
+  const heroItem1 = document.getElementById("jsHeroItem1");
+  const heroItem2 = document.getElementById("jsHeroItem2");
+  const heroItem3 = document.getElementById("jsHeroItem3");
+  const heroItem4 = document.getElementById("jsHeroItem4");
+  const heroItem5 = document.getElementById("jsHeroItem5");
+  const heroItem6 = document.getElementById("jsHeroItem6");
+  const heroItem7 = document.getElementById("jsHeroItem7");
+  const heroItem8 = document.getElementById("jsHeroItem8");
+
+  //hero item color hover on btm effect
+
+  //   hero item button depress and return
+  //hero item 1
+  heroItem1.addEventListener("mouseenter", function () {
+    $(heroItem1).animate(
+      {
+        bottom: -50,
+      },
+      500
+    );
+  });
+
+  heroItem1.addEventListener("mouseleave", function () {
+    $(heroItem1).animate(
+      {
+        bottom: 0,
+      },
+      500
+    );
+  });
+
+  //hero item2
+
+  heroItem2.addEventListener("mouseenter", function () {
+    $(heroItem2).animate(
+      {
+        bottom: -50,
+      },
+      500
+    );
+  });
+
+  heroItem2.addEventListener("mouseleave", function () {
+    $(heroItem2).animate(
+      {
+        bottom: 0,
+      },
+      500
+    );
+  });
+
+  //hero item3
+
+  heroItem3.addEventListener("mouseenter", function () {
+    $(heroItem3).animate(
+      {
+        bottom: -50,
+      },
+      500
+    );
+  });
+
+  heroItem3.addEventListener("mouseleave", function () {
+    $(heroItem3).animate(
+      {
+        bottom: 0,
+      },
+      500
+    );
+  });
+
+  //hero item4
+
+  heroItem4.addEventListener("mouseenter", function () {
+    $(heroItem4).animate(
+      {
+        bottom: -50,
+      },
+      500
+    );
+  });
+
+  heroItem4.addEventListener("mouseleave", function () {
+    $(heroItem4).animate(
+      {
+        bottom: 0,
+      },
+      500
+    );
+  });
+
+  //hero item5
+
+  heroItem5.addEventListener("mouseenter", function () {
+    $(heroItem5).animate(
+      {
+        bottom: -50,
+      },
+      500
+    );
+  });
+
+  heroItem5.addEventListener("mouseleave", function () {
+    $(heroItem5).animate(
+      {
+        bottom: 0,
+      },
+      500
+    );
+  });
+
+  //hero item6
+
+  heroItem6.addEventListener("mouseenter", function () {
+    $(heroItem6).animate(
+      {
+        bottom: -50,
+      },
+      500
+    );
+  });
+
+  heroItem6.addEventListener("mouseleave", function () {
+    $(heroItem6).animate(
+      {
+        bottom: 0,
+      },
+      500
+    );
+  });
+
+  //hero item7
+
+  heroItem7.addEventListener("mouseenter", function () {
+    $(heroItem7).animate(
+      {
+        bottom: -50,
+      },
+      500
+    );
+  });
+
+  heroItem7.addEventListener("mouseleave", function () {
+    $(heroItem7).animate(
+      {
+        bottom: 0,
+      },
+      500
+    );
+  });
+
+  //hero item8
+
+  heroItem8.addEventListener("mouseenter", function () {
+    $(heroItem8).animate(
+      {
+        bottom: -50,
+      },
+      500
+    );
+  });
+
+  heroItem8.addEventListener("mouseleave", function () {
+    $(heroItem8).animate(
+      {
+        bottom: 0,
+      },
+      500
+    );
+  });
+});
